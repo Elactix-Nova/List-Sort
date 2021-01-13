c041ae0285ae61f583320e5c8486075b75624ce8
smart-aditya <31545372+smart-aditya@users.noreply.github.com>
2021-01-13 11:47:57 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+L=[]
+a=input('Enter numbers to be added to list: ')
+L=list(map(int,a.split()))
+for i in range(len(L)-1):
+  for j in range(len(L)-i-1):
+    if L[j]>L[j+1]:
+      L[j],L[j+1]=L[j+1],L[j]
+print(L)
